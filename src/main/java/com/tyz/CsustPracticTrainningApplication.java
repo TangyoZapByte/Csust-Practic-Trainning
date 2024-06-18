7539fdfa04512bc5647aceb53aeac11e1a41d002
TangyoZapByte <tangyizuo@gmail.com>
2024-06-18 16:11:10 +0800

    csust-practic-trainning

---
+++ src/main/java/com/tyz/CsustPracticTrainningApplication.java
@@ -0,0 +1,13 @@
+package com.tyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsustPracticTrainningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsustPracticTrainningApplication.class, args);
+    }
+
+}
